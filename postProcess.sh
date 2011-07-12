7b1fdac12b8c30a7eb37491aed8ac2f11894eee9
Josh Bialkowski <jbialk@mit.edu>
2011-07-12 19:09:17 -0400

    went back to distribution (not trunk) because delayed assignment of line attributes eliminates the need

--- postProcess.sh
+++ postProcess.sh
@@ -1,6 +1,6 @@
-LATEXMLPOST=/home/josh/Codes/devroot/usr/bin/latexmlpost
-export PERL5LIB=/home/josh/Codes/devroot/usr/share/perl/5.10.1/
+#LATEXMLPOST=/home/josh/Codes/devroot/usr/bin/latexmlpost
+#export PERL5LIB=/home/josh/Codes/devroot/usr/share/perl/5.10.1/
 
-#LATEXMLPOST=latexmlpost
+LATEXMLPOST=latexmlpost
 
 $LATEXMLPOST --novalidate --stylesheet=LaTeXML-xhtml.xsl --destination=test.xhtml --css=core.css --css=algorithm2e.css test.xml
