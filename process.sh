163951408dc7aa14b0ac07971c8859048b288f23
Josh Bialkowski <jbialk@mit.edu>
2011-07-12 17:28:13 -0400

    removed extra lines at the end of blocks

--- process.sh
+++ process.sh
@@ -1,1 +1,5 @@
-latexml --destination=test.xml test.tex
+
+LATEXML=latexml
+#LATEXML=/home/josh/Codes/devroot/usr/bin/latexml
+#export PERL5LIB=/home/josh/Codes/devroot/usr/share/perl/5.10.1/
+$LATEXML --destination=test.xml test.tex
