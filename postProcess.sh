84a3e9ebe0593af35e8259f6088f74b4e6ec06ac
Josh Bialkowski <jbialk@mit.edu>
2011-07-12 17:40:43 -0400

    switched to trunk

--- postProcess.sh
+++ postProcess.sh
@@ -1,1 +1,6 @@
-latexmlpost --novalidate --stylesheet=LaTeXML-xhtml.xsl --destination=test.xhtml --css=core.css --css=algorithm2e.css test.xml
+LATEXMLPOST=/home/josh/Codes/devroot/usr/bin/latexmlpost
+export PERL5LIB=/home/josh/Codes/devroot/usr/share/perl/5.10.1/
+
+#LATEXMLPOST=latexmlpost
+
+$LATEXMLPOST --novalidate --stylesheet=LaTeXML-xhtml.xsl --destination=test.xhtml --css=core.css --css=algorithm2e.css test.xml
