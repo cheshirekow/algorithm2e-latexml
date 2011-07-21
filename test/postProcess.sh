9ae3c1a00b68fef01811661bc2bce41eda4ec3c4
Josh Bialkowski <jbialk@mit.edu>
2011-07-21 11:48:40 -0400

    fixed make scripts

--- test/postProcess.sh
+++ test/postProcess.sh
@@ -3,4 +3,9 @@
 
 LATEXMLPOST=latexmlpost
 
-$LATEXMLPOST --novalidate --stylesheet=LaTeXML-xhtml.xsl --destination=test.xhtml --css=core.css --css=algorithm2e.css test.xml
+$LATEXMLPOST --novalidate \
+                --stylesheet=../algorithm2e/LaTeXML-xhtml.xsl \
+                --destination=test.xhtml \
+                --css=core.css \
+                --css=../algorithm2e/algorithm2e.css \
+                test.xml
