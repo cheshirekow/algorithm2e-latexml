45743c54db11c010ea3699f41b072127986ddc37
Josh Bialkowski <jbialk@mit.edu>
2011-09-25 11:28:59 -0400

    changed scripts to have separate trunk / release tests

---
+++ test/postProcess_trunk.sh
@@ -0,0 +1,11 @@
+LATEXMLPOST=/home/josh/Codes/devroot/usr/bin/latexmlpost
+export PERL5LIB=/home/josh/Codes/devroot/usr/share/perl/5.10.1/
+
+#LATEXMLPOST=latexmlpost
+
+$LATEXMLPOST --novalidate \
+                --stylesheet=../algorithm2e/LaTeXML-xhtml.xsl \
+                --destination=test.xhtml \
+                --css=core.css \
+                --css=../algorithm2e/algorithm2e.css \
+                test.xml
---
+++ test/process_trunk.sh
@@ -0,0 +1,6 @@
+LATEXML=/home/josh/Codes/devroot/usr/bin/latexml
+export PERL5LIB=/home/josh/Codes/devroot/usr/share/perl/5.10.1/
+
+#LATEXML=latexml
+
+$LATEXML --path=../algorithm2e --destination=test.xml test.tex
