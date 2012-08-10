9f9f98f3291915245db12a5208619c47f7fc4681
Josh Bialkowski <jbialk@mit.edu>
2012-08-09 20:32:39 -0400

    more weirdness on order of digestion

--- algorithm2e/test.pl
+++ algorithm2e/test.pl
@@ -14,3 +14,19 @@ for my $name('bob','joan','brian')
 $subs{'bob'}->();
 $subs{'joan'}->();
 $subs{'brian'}->();
+
+print "\n\n";
+
+$href_a = {'a'=>'a','b'=>'b','c'=>'c'};
+$href_b = {};
+
+%{href_b} = %{href_a};
+
+$href_b->{'a'} = 'd';
+$href_b->{'b'} = 'e';
+$href_b->{'c'} = 'f';
+
+for $key( qw(a b c) )
+{
+    printf( "href_a: %s    href_b: %s\n", $href_a->{$key}, $href_b->{$key});
+}
